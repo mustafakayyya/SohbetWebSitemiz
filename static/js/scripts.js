52d9991ff8e86f95e18a951e14a97d2f9676b031
mustafakayyya <j3339109@gmail.com>
2024-08-05 15:52:48 +0300

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var socket = io();
+
+    document.getElementById('messageForm').addEventListener('submit', function (e) {
+        e.preventDefault();
+        var messageInput = document.getElementById('message');
+        socket.send(messageInput.value);
+        messageInput.value = '';
+    });
+
+    socket.on('message', function (msg) {
+        var messagesDiv = document.getElementById('messages');
+        var messageElement = document.createElement('div');
+        messageElement.className = 'message ' + (msg.user === "{{ username }}" ? 'own-message' : 'other-message');
+        messageElement.innerHTML = '<strong>' + msg.user + ':</strong> ' + msg.text;
+        messagesDiv.appendChild(messageElement);
+        messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    });
+
     const themeSelect = document.getElementById('theme');
     themeSelect.addEventListener('change', function () {
         document.body.className = themeSelect.value;
